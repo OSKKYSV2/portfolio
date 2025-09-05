a8c0101cdfede70ed29bf6e90141ab27c57b42f1
OSKKYSV2 <okinek321@seznam.cz>
2025-09-05 14:46:39 +0200

    a

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
 };
 
 export default nextConfig;
+
+
+
+
