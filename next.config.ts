cc1ce6a31df8d19bfc12628caf13d1ee94dcbe8f
OSKKYSV2 <okinek321@seznam.cz>
2025-09-08 09:52:57 +0200

    backend

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",          // nativní statický export v Next.js 15
+  output: 'standalone',          // nativní statický export v Next.js 15
   reactStrictMode: true,
   images: {
     unoptimized: true,       // vypnutí image optimalizace
