c592818c48fd9f74671657abaf0e32f57a3cc1fa
OSKKYSV2 <okinek321@seznam.cz>
2025-09-08 10:20:56 +0200

    Fix GitHub API ENV vars

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',          // nativní statický export v Next.js 15
+  output: "standalone", // serverové buildy (NE 'export')
   reactStrictMode: true,
-  images: {
-    unoptimized: true,       // vypnutí image optimalizace
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
 };
 
 export default nextConfig;
